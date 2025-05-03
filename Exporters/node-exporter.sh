31f166cabd971c3c0dae5a5a0de2de156449f0b1
khashayar panbeian <52905588+khpcoding@users.noreply.github.com>
2025-05-03 18:57:45 +0330

    Update node-exporter.sh

--- Exporters/node-exporter.sh
+++ Exporters/node-exporter.sh
@@ -1,10 +1,11 @@
 # Installation node exporter
 #!/bin/bash
-export RELEASE="1.8.2"
+export RELEASE="1.9.0"
 wget https://github.com/prometheus/node_exporter/releases/download/v$RELEASE/node_exporter-$RELEASE.linux-386.tar.gz
 tar xzf node_exporter-$RELEASE.linux-386.tar.gz
 cd node_exporter-$RELEASE.linux-386/
 mv ./node_exporter /usr/local/bin/
+chown node_exporter:node_exporter /usr/local/bin/
 useradd --no-create-home --shell /bin/false node_exporter
 
 echo  -n "
@@ -22,4 +23,6 @@ WantedBy=multi-user.target " >  /etc/systemd/system/node_exporter.service
 
 
 systemctl daemon-reload
+systemctl start node_exporter.service
 systemctl enable node_exporter.service
+systemctl status node_exporter.service
